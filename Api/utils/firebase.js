792a5c333c1ad3a755f048b2ccf084bdde3d577e
ggonu20 <g.gonzalezn@utem.cl>
2023-11-22 20:18:16 -0300

    arreglado problema

--- Api/utils/firebase.js
+++ Api/utils/firebase.js
@@ -1,11 +1,12 @@
 // app/utils/firebase.js
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./path/to/your/firebase/serviceAccountKey.json');
+const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   // Add other Firebase configurations if needed
 });
 
+console.log(`firebase iniciado`);
 module.exports = admin;
